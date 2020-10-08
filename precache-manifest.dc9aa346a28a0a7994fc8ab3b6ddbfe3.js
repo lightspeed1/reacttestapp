b12dc143cf75b3e910c2682f1dfc6ec1c81dc030
Andy Strong <strong.andrew.j@gmail.com>
2020-10-07 22:29:42 -0600

    Updates

---
+++ precache-manifest.dc9aa346a28a0a7994fc8ab3b6ddbfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f79fac8af6eadb774b8783f095c2a8",
+    "url": "/reacttestapp/index.html"
+  },
+  {
+    "revision": "2f950a541fa9de361b6e",
+    "url": "/reacttestapp/static/css/main.4b14defa.chunk.css"
+  },
+  {
+    "revision": "e6e53dc861e99a833883",
+    "url": "/reacttestapp/static/js/2.2534c58f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reacttestapp/static/js/2.2534c58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f950a541fa9de361b6e",
+    "url": "/reacttestapp/static/js/main.b609e450.chunk.js"
+  },
+  {
+    "revision": "17c3171618f437d48c41",
+    "url": "/reacttestapp/static/js/runtime-main.72b295ad.js"
+  }
+]);
